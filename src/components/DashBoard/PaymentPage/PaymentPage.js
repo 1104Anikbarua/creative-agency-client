50d40cf83887c667cc4574779f7c4fc51462b3a7
Anik Barua <anikbb04@gmail.com>
2023-02-03 13:59:04 +0600

    payment method implemented

--- src/components/DashBoard/PaymentPage/PaymentPage.js
+++ src/components/DashBoard/PaymentPage/PaymentPage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import { Link, useParams } from 'react-router-dom';
+import LoadingSpinner from '../../Shared/LoadingSpinner/LoadingSpinner';
 
 const PaymentPage = () => {
     const param = useParams();
     const { productId } = param;
     console.log(productId)
-    const { data: service, isLoading } = useQuery(['order', productId], () => fetch(`http://localhost:5000/v1/order/${productId}`))
+    const { data: service, isLoading } = useQuery(['order', productId], () => fetch(`http://localhost:5000/v1/order/${productId}`));
+    if (isLoading) {
+        return <LoadingSpinner></LoadingSpinner>
+    }
     const paid = true;
     return (
         <div className='w-full h-screen bg-success'>
@@ -17,19 +21,19 @@ const PaymentPage = () => {
                     <div className='w-[415px] h-[254px] text-center shadow-2xl p-6 border-2 border-gray-400 rounded-[20px] bg-white'>
                         <div className='flex items-center'>
                             <div>
-                                <img className='w-[74px] h-[74px] mr-[184px]' src={service.projectImage} alt="" />
+                                <img className='w-[74px] h-[74px] mr-[184px]' src={service?.projectImage} alt="" />
 
                             </div>
                             <div>
                                 {paid ?
-                                    <Link to={`${service._id}`}><button className='w-[108px] h-[40px] bg-error rounded text-green-400 text-base' style={{ backgroundColor: '#C6FFE0' }}>Download</button></Link> :
-                                    <Link to={`${service._id}`}>
+                                    <Link to={`${service?._id}`}><button className='w-[108px] h-[40px] bg-error rounded text-green-400 text-base' style={{ backgroundColor: '#C6FFE0' }}>Download</button></Link> :
+                                    <Link to={`${service?._id}`}>
                                         <button className='w-[108px] h-[40px] bg-error rounded text-red-400 text-base'>Pay</button></Link>}
 
                             </div>
                         </div>
-                        <h1 className='font-semibold text-xl py-[18px] text-secondary'>{service.projectName}</h1>
-                        <p className='text-base text-slate-400' title={service.projectDescription}>{service.projectDescription?.slice(0, 50)}{service.projectDescription.length > 50 ? '...' : '.'}</p>
+                        <h1 className='font-semibold text-xl py-[18px] text-secondary'>{service?.projectName}</h1>
+                        <p className='text-base text-slate-400' title={service?.projectDescription}>{service?.projectDescription?.slice(0, 50)}{service?.projectDescription?.length > 50 ? '...' : '.'}</p>
                     </div>
                     {/* )
                  } */}
